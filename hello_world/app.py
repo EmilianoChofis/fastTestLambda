4a14e16ebc1894ac14f17bf3f18f34c5c47d6361
emilianoa3e <emiliano.ortiz@a3e.com.mx>
2024-05-09 13:00:59 -0600

    agregado de except y cambio de query

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,12 +2,7 @@ import json
 
 from hello_world.common.utils import db_connection
 
-
-# import requests
-
-
 def lambda_handler(event, context):
-
     """
     :param event:
     :param context:
@@ -17,17 +12,19 @@ def lambda_handler(event, context):
     grade = event['grade']
 
     connection = db_connection()
-
     try:
         with connection.cursor() as cursor:
-            sql = f"DELETE alumnos WHERE grade = {grade} and group={group}"
+            sql = "DELETE FROM alumnos WHERE grado = %s AND grupo = %s"
             cursor.execute(sql, (grade, group))
             connection.commit()
             return {
                 'statusCode': 200,
-                'body': json.dumps("Eliminado")
+                'body': json.dumps("Registro eliminado")
             }
-
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Error: {str(e)}")
+        }
     finally:
         connection.close()
-
